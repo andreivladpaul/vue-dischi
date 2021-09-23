b5af4af4a89165926c405fd0d08906249d0f9583
Vlad <andreivladpaul@gmail.com>
2021-09-23 14:01:04 +0200

    barra di ricerca

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,12 @@
 <template>
-  <div class="header px-3 py-2">
+  <div class="header px-3 py-2 d-flex justify-content-between">
       <img src="../assets/spotify-logo.png" alt="">
+
+      <div class="search d-flex justify-content-center align-items-center"  >
+          <input type="text" class="px-1 me-2 rounded-1"  placeholder="Search by genre">
+          <button type="input" class="px-3 py-1 me-2 rounded-1">Search</button>
+
+      </div>
   </div>
 </template>
 
@@ -15,11 +21,29 @@ export default {
     .header {
         width: 100vw;
         background-color: $light-greysh;
+        
 
         img {
             width: 50px;
             height: 50px;
         }
+        .search {
+            input {
+                
+                &::placeholder {
+                    font-size: 16px;
+                }
+           
+            }
+             button {
+                    background-color: rgb(10, 180, 101);
+                    color: #fff;
+                    border: none;
+                    font-size: 16px;
+            }
+        }
+
+
         
     }
 
--- src/components/SingleDisc.vue
+++ src/components/SingleDisc.vue
@@ -1,11 +1,11 @@
 <template>
-  <div class="disc text-center pt-2 ">
+  <div class="disc text-center pt-3 ">
       <a href="#">
           <img class="img-fluid mb-1" :src="info.poster" :alt="info.title">
       </a>
-      <p class="f-big mb-1 title text-uppercase font-weight-bold text-white">{{ info.title }}</p>
+      <p class="title f-big mb-1  text-uppercase font-weight-bold text-white">{{ info.title }}</p>
         <p class="author f-med mb-1">{{ info.author }}</p>
-        <p class="fw-bold f-sm mb-1 year">{{ info.year }}</p>
+        <p class=" year fw-bold f-sm mb-1 ">{{ info.year }}</p>
   </div>
 </template>
 
